27c05779fea5d361d20a3abecd3fc6286ed01a07
Kern Michel <michel.kern@ti8m.ch>
2017-12-05 09:32:39 +0100

    Add solution for 5

---
+++ 2017/5/5.2.js
@@ -0,0 +1,17 @@
+let fs = require("fs");
+
+fs.readFile("captcha", "utf8", (err, data) => {
+    let instructions = data.split("\n");
+    let pointer = 0;
+    let steps = 0;
+
+    while(pointer > -1 && pointer < instructions.length){
+        let offset = Number(instructions[pointer]);
+        instructions[pointer] = offset >= 3 ? offset -1 : offset + 1;
+        pointer = pointer + offset;
+        steps++;
+    }
+
+    console.log(steps);
+});
+
