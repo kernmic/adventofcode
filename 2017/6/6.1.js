8e68610b04a2cf20d048d1c094ced042afb1a498
Michel Kern <michel@kern-it.ch>
2017-12-06 08:06:09 +0100

    Add solution for 6

---
+++ 2017/6/6.1.js
@@ -0,0 +1,34 @@
+let fs = require("fs");
+
+let getBankToDistribute = (memoryBanks) => {
+    let largestValue = memoryBanks.reduce((acc, val) => {
+        return val > acc ? val : acc;
+    },0);
+    return memoryBanks.findIndex(element => element == largestValue);
+}
+
+let distributeBank = (bank, memoryBanks) => {
+    let distribute = memoryBanks[bank];
+    memoryBanks[bank] = 0;
+    while(distribute > 0){
+        bank++;
+        bank = (bank == memoryBanks.length) ? 0 : bank;
+        memoryBanks[bank]++;
+        distribute--;
+    }
+};
+
+fs.readFile("captcha", "utf8", (err, data) => {
+    let memoryBankHistory = [];
+    let memoryBanks = data.split("\t").map(val => Number(val));
+    let steps = 0;
+    while(!memoryBankHistory.some(el => {
+        return JSON.stringify(el)==JSON.stringify(memoryBanks);
+    })){
+        memoryBankHistory.push(memoryBanks.slice());
+        distributeBank(getBankToDistribute(memoryBanks),memoryBanks);
+        steps++;
+    }
+
+    console.log(steps);
+});
