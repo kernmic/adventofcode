1dffa018a19313f14d14e9c2f473863e72852313
Michel Kern <michel@kern-it.ch>
2017-12-08 13:16:31 +0100

    Shorten 8

--- 2017/8/8.1.js
+++ 2017/8/8.1.js
@@ -1,30 +1,17 @@
-let fs = require("fs");
-
-let replaceOperators = (register) => {
-    return register.replace(/\sinc\s/," += ").replace(/\sdec\s/," -= ");
-};
-
-let initRegisters = (register) => {
-    let name = register[0].split(/\s/)[0];
-    let command = ""+name+" = 0;";
-    register[2] = name;
-    eval(command);
-    return register;
-}
-
-fs.readFile("captcha", "utf8", (err, data) => {
+require("fs").readFile("captcha", "utf8", (err, data) => {
     let registerMap = [];
-    let biggestValue = data.split("\n")
+    console.log(data.split("\n")
         .map(register => register.split(/\sif\s/))
-        .map(register => initRegisters(register))
-        .map(register => register.map(reg => replaceOperators(reg)))
         .map(register => {
-            if(eval(register[1])){
-                registerMap[register[2]] = eval(register[0]);
-            }
+            register[2] = register[0].split(/\s/)[0];
+            eval(""+register[2]+" = 0;");
+            return register;
+        })
+        .map(register => register.map(reg => reg.replace(/\sinc\s/," += ").replace(/\sdec\s/," -= ")))
+        .map(register => {
+            if(eval(register[1])) registerMap[register[2]] = eval(register[0]);
             return register;
         }).reduce((acc, val) => {
             return registerMap[val[2]] > acc ? registerMap[val[2]] : acc;
-        }, 0);
-        console.log(biggestValue);
+        }, 0));
 });
--- 2017/8/8.2.js
+++ 2017/8/8.2.js
@@ -1,32 +1,17 @@
-let fs = require("fs");
-let registerMap = [];
-
-let replaceOperators = (register) => {
-    return register.replace(/\sinc\s/," += ").replace(/\sdec\s/," -= ");
-};
-
-let initRegisters = (register) => {
-    let name = register[0].split(/\s/)[0];
-    let command = ""+name+" = 0;";
-    register[2] = name;
-    registerMap[name] = 0;
-    eval(command);
-    return register;
-}
-
-fs.readFile("captcha", "utf8", (err, data) => {
-    let biggestValue = data.split("\n")
+require("fs").readFile("captcha", "utf8", (err, data) => {
+    let registerMap = [];
+    console.log(data.split("\n")
         .map(register => register.split(/\sif\s/))
-        .map(register => initRegisters(register))
-        .map(register => register.map(reg => replaceOperators(reg)))
         .map(register => {
-            if(eval(register[1])){
-                let result = eval(register[0]);
-                registerMap[register[2]] = result > registerMap[register[2]] ? result : registerMap[register[2]];
-            }
+            register[2] = register[0].split(/\s/)[0];
+            eval(""+register[2]+" = 0;");
             return register;
+        })
+        .map(register => register.map(reg => reg.replace(/\sinc\s/," += ").replace(/\sdec\s/," -= ")))
+        .map(register => {
+            if(eval(register[1])) registerMap[register[2]] = eval(register[0]);
+            return registerMap[register[2]];
         }).reduce((acc, val) => {
-            return registerMap[val[2]] > acc ? registerMap[val[2]] : acc;
-        }, 0);
-        console.log(biggestValue);
+            return val > acc ? val : acc;
+        }, 0));
 });
