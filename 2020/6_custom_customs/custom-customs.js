2c25d01c8527d9d75db2f349ae55f67e779d089e
Michel Kern <michel@kern-it.ch>
2020-12-06 10:08:51 +0100

    Tiny cleanup

--- 2020/6_custom_customs/custom-customs.js
+++ 2020/6_custom_customs/custom-customs.js
@@ -1,37 +1,35 @@
-const alphabet = new Array('z'.charCodeAt(0)-'a'.charCodeAt(0)+1)
-        .fill(null)
-        .map((_,idx)=>String.fromCharCode('a'.charCodeAt(0) + idx));
+const alphabet = new Array('z'.charCodeAt(0) - 'a'.charCodeAt(0) + 1)
+    .fill(null)
+    .map((_, idx) => String.fromCharCode('a'.charCodeAt(0) + idx));
 
-const getQuestionsAnyoneAnsweredYes = (input) => {
-  return [...input,''].reduce(({questions, count},val,idx,arr) =>
-      !val
-      ? ({
+const getQuestionsAnyoneAnsweredYes = (input) =>
+    [...input, ''].reduce(({questions, count}, val) =>
+        !val
+            ? ({
+                questions: new Set(),
+                count: count + questions.size
+            })
+            : ({
+                questions: new Set([...questions, ...val.split("")]),
+                count
+            }), {
         questions: new Set(),
-        count: count + questions.size
-      })
-      : ({
-      questions: new Set([...questions, ...val.split("")]),
-      count
-    }), {
-    questions: new Set(),
-    count: 0
-  }).count;
-};
+        count: 0
+    }).count;
 
-const getQuestionsEveryoneAnsweredYes = (input) => {
-  return [...input,''].reduce(({questions, count},val,idx,arr) =>
-      !val
-      ? ( {
+const getQuestionsEveryoneAnsweredYes = (input) =>
+    [...input, ''].reduce(({questions, count}, val) =>
+        !val
+            ? ({
+                questions: alphabet,
+                count: count + questions.length
+            }) : ({
+                questions: val.split("").filter(x => questions.includes(x)),
+                count
+            }), {
         questions: alphabet,
-        count: count + questions.length
-      }) : ({
-      questions: val.split("").filter(x => questions.includes(x)),
-      count
-    }), {
-    questions: alphabet,
-    count: 0
-  }).count;
-};
+        count: 0
+    }).count;
 
 exports.getQuestionsEveryoneAnsweredYes = getQuestionsEveryoneAnsweredYes;
 exports.getQuestionsAnyoneAnsweredYes = getQuestionsAnyoneAnsweredYes;
