86dce9e2bf31fdcf85d02c6fa959a961572ee09d
Michel Kern <michel@kern-it.ch>
2020-12-13 17:28:57 +0100

    WIP

--- 2020/13_shuttle_search/shuttle-search.js
+++ 2020/13_shuttle_search/shuttle-search.js
@@ -1,9 +1,10 @@
-const transformData = input => [+input[0],input[1].split(",").filter(x => x !== "x").map(x => +x).sort((a,b)=>a-b)]
-
 const part1 = (input) => {
-  const [depart, laneIds] = transformData(input);
+  const [depart, laneIds] = [+input[0],input[1].split(",").filter(x => x !== "x").map(x => +x).sort((a,b)=>a-b)];
 
-  const { laneId, wait } = laneIds.map(laneId => depart%laneId).map((rest,id) => laneIds[id] - rest).reduce((acc,waitMin,idx) => waitMin < acc.wait ?
+  const { laneId, wait } = laneIds
+      .map(laneId => depart%laneId)
+      .map((rest,id) => laneIds[id] - rest)
+      .reduce((acc,waitMin,idx) => waitMin < acc.wait ?
       ({
         laneId: laneIds[idx],
         wait: waitMin
@@ -15,4 +16,45 @@ const part1 = (input) => {
   return laneId * wait;
 };
 
+// const getOffsetForTimestamp = (t, laneIds) => {
+//     return laneIds
+//         .map(laneId => isNaN(+laneId) ? laneId : t%laneId)
+//         .map((rest,id) => {
+//             if()
+//             console.log(id);
+//             return rest === 0 ? 0 : isNaN(+rest) ? id : laneIds[id] - rest;
+//         })
+// }
+
+const   getOffsetForTimestamp = (t, laneIds) => {
+    return laneIds
+        .map((laneId,id) => {
+            if(isNaN(+laneId)){
+                return id;
+            }
+            const rest = t%laneId ? (laneIds[id] - (t%laneId)) : 0;
+            if(rest !== id){
+                throw new Error("not ascending");
+            }
+            return rest;
+        })
+}
+
+const part2 = (input) => {
+    const laneIds = input[1].split(",").map(x => isNaN(+x) ? x : +x);
+    let i = laneIds[0];
+    while(true){
+        try{
+            let b = i%20;
+            getOffsetForTimestamp(b,laneIds);
+            return i;
+        } catch(e){
+        } finally {
+            i+=laneIds[0];
+        }
+    }
+}
+
+exports.getOffsetForTimestamp = getOffsetForTimestamp;
 exports.part1 = part1;
+exports.part2 = part2;
--- 2020/13_shuttle_search/solve.js
+++ 2020/13_shuttle_search/solve.js
@@ -1,8 +1,11 @@
-const { part1 } = require("./shuttle-search");
+const { part1, part2 } = require("./shuttle-search");
 const { parseInput } = require("../0_util/input-parser");
 
 const input = parseInput();
 
 const sol1 = part1(input);
+const sol2 = part2(input);
 console.log("Solution1");
 console.log(sol1);
+console.log("Solution2");
+console.log(sol2);
