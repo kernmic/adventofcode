91209f21537fd59abe7dafb28be30d17c21f7561
Michel Kern <michel.kern@timewise.ch>
2020-12-14 10:32:38 +0100

    WIP 2020 14 2

--- 2020/14_docking_data/docking-data.js
+++ 2020/14_docking_data/docking-data.js
@@ -38,5 +38,53 @@ const part1 = (input) => {
   return +Object.values(reduced.memory).reduce((acc,val)=>acc.add(val),Long.fromInt(0)).toString()
 };
 
+const applyMaskPart2 = (mask,val) => {
+  const value = Long.fromInt(val);
+  const orMask = Long.fromString(mask.replace(/[X]/g,'0'),2);
+  return value.or(orMask);
+}
+
+const getPossibleLocations = (mask,val) => {
+  const nrOfX = mask.split("X").length - 1;
+  const nrOfCombs = parseInt(new Array(nrOfX).fill("1").join(""),2);
+  const combs = [val.toString(2)];
+  for(let i = 0;i<=nrOfCombs;i++){
+    combs.push(i.toString(2));
+  }
+  return combs;
+}
+
+
+const part2 = (input) => {
+  const data = input.map(d => d.split(/\s=\s/));
+  const reduced = data.reduce(({ acc, mask, memory },[instr, val]) => {
+    if(instr === "mask"){
+      return {
+        acc,
+        memory,
+        mask: val
+      }
+    }
+    const mem = parseMemoryLocation(instr);
+    const memAfterMask = applyMaskPart2(mask,mem);
+    const possibleLocations = getPossibleLocations(mask,memAfterMask);
+    console.log({ mem,memAfterMask: memAfterMask.toString(), bin: memAfterMask.toString(2), possibleLocations});
+    return {
+      acc,
+      mask,
+      memory: {
+        ...memory,
+        [mem]: +val
+      }
+    }
+  }, {
+    mask: 0,
+    acc: 0,
+    memory: {}
+  })
+  console.log(reduced)
+  return +Object.values(reduced.memory).reduce((acc,val)=>acc.add(val),Long.fromInt(0)).toString()
+};
 
 exports.part1 = part1;
+exports.part2 = part2;
--- 2020/14_docking_data/docking-data.test.js
+++ 2020/14_docking_data/docking-data.test.js
@@ -1,8 +1,12 @@
-const { part1 } = require("./docking-data");
+const { part1, part2 } = require("./docking-data");
 const { parseInput } = require("../0_util/input-parser");
 let path = require("path");
 
 test("solves correctly", () => {
   const input = parseInput(path.resolve(__dirname, "test1.txt"));
   expect(part1(input)).toBe(165);
+});
+test("solves correctly part 2", () => {
+  const input = parseInput(path.resolve(__dirname, "test2.txt"));
+  expect(part2(input)).toBe(208);
 });
